6ebc2fd4c5f1ff689fa9bdf8d46d6db01f4ce5c2
samtarey <samidha.tarey@hotmail.com>
2015-03-09 14:29:14 -0700

    Add my files to my repository

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Add Hover effect to menus
+jQuery('ul.nav ul.dropdown').hover(function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+}, function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+});
